2aeb85988b60b112f1db6a93f6e78b1e0c5b4ecd
Ashishxo <dhiwarashish4@gmail.com>
2024-02-23 15:05:00 +0530

    mobile responsive

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -14,13 +14,13 @@ function Todos(){
             <div className="flex flex-col items-center gap-2">
           
               {todos.map((todo) =>(
-                <form key={todo.id} className="bg-[#efefef] w-5/6 flex mt-4 py-4 px-3 rounded-3xl justify-between">
+                <form key={todo.id} className="bg-[#efefef] w-full md:w-5/6 flex mt-4 py-4 px-3 rounded-3xl justify-between">
 
                     
                         <div className="flex gap-6 items-center ml-3 w-full">
                             <input type="checkbox" name="" id="" className="size-4" value={todo.completed} onChange={() => dispatch(toggleTodo(todo.id))}/>
                             {/* <h2 className={`font-['Quicksand'] text-xl ${todo.completed ? `line-through` : ``}`}>{todo.text}</h2> */}
-                            <input type="text" value={todo.text} onChange={(e) => dispatch(updateTodo({id: todo.id, text: e.target.value}))} disabled={!todo.isEditable} className={`w-full block font-['Quicksand'] rounded-lg px-2 text-xl ${todo.completed ? `line-through` : ``} text-ellipsis bg-inherit ${todo.isEditable ? `border-solid border-2 border-gray-500`:``} ${todo.id}`}/>
+                            <input type="text" value={todo.text} onChange={(e) => dispatch(updateTodo({id: todo.id, text: e.target.value}))} disabled={!todo.isEditable} className={`w-full block font-['Quicksand'] rounded-lg px-2 md:text-xl ${todo.completed ? `line-through` : ``} text-ellipsis bg-inherit ${todo.isEditable ? `border-solid border-2 border-gray-500`:``} ${todo.id}`}/>
                         </div>
 
                         <div className="px-1 flex gap-3">
